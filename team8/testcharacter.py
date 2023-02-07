14487145269e3a872e2141ddca7f8206d90880e6
Alex <strickland7712@gmail.com>
2023-02-06 20:24:33 -0500

    added bomb dropping

--- team8/testcharacter.py
+++ team8/testcharacter.py
@@ -4,137 +4,212 @@ import sys
 sys.path.insert(0, '../bomberman')
 # Import necessary stuff
 from entity import CharacterEntity
+from sensed_world import SensedWorld
 from colorama import Fore, Back
 import math
 
 class TestCharacter(CharacterEntity):
     move_count = 0
     path_plan = True 
-    
+    path = []
+    depth = 3
+    bomb_position = None
+    timer = 0
+
     def do(self, wrld):
 
-        status = self.check_3_spaces(wrld)
+        monster_prox = self.is_monster_in_proximity(wrld)
 
-        if (status != False):
-            print('running')
-            self.run(wrld, status)
+        if self.move_count == len(self.path):
+            
+            print('finished path')
+            self.path_plan = True
+
+        if  monster_prox[0] == True:
+
+            print('monster in proximity')
+            print(monster_prox)
+            if(not self.bomb_position):
+                self.place_bomb()
+                self.bomb_position = self.x, self.y
+            print('bomb: ', self.bomb_position)
+            best_move = self.run_expectimax(wrld,monster_prox[1])
+            self.path.clear()
+            self.path.append(best_move)
+            print('path: ', self.path)
+            self.move_count = 0   
 
         elif self.path_plan:
-            print('planning')
-            start = (self.x,self.y)
-            goal = wrld.exitcell
-            #self.move_count = 0
-            came_from, cost_inpycurred = self.A_star(wrld,start,goal)
-            #print(came_from)
-            #self.path = None
-            self.path = self.get_path(came_from, start, goal)
-            print(self.path)
-            self.path_plan = False
-            dx, dy = self.extract_move(self.path[self.move_count])
-            self.set_cell_color(self.path[self.move_count][0], self.path[self.move_count][1], Back.RED)
-            print('move_count: ', self.move_count)
-            print('move: ', dx, dy)
-            self.move(dx, dy)
-            self.move_count+=1
 
-        else:
-            
-            if (status == False):
-                print('on path')
-                dx, dy = self.extract_move(self.path[self.move_count])
+            print('planning path')
+            came_from, cost_incurred = self.A_star(wrld)
+            self.path = self.get_path(came_from, wrld)
+            self.path_plan = False
+            self.move_count = 0
+
+        if self.path != []:
+
+            print('path not complete')
+            dx, dy = self.extract_move(self.path[self.move_count])
+            print(dx, dy)
+            print('bomb: ', self.bomb_position)
+            if(self.blast_radius(self.bomb_position, (self.x + dx, self.y + dy))):
+                print('stopped')
+                self.move(0, 0)
+                
+            else:
+                
+                print('move: ', dx, dy) 
                 self.set_cell_color(self.path[self.move_count][0], self.path[self.move_count][1], Back.RED)
-                print('move_count: ', self.move_count)
-                print('move: ', dx, dy)
                 self.move(dx, dy)
                 self.move_count+=1
+        
+        print(self.timer)
+        if(self.bomb_position):    
+            self.timer += 1
+        if self.timer >= 5:
+            self.bomb_position = None
+            self.timer = 0 
+
+    def run_expectimax(self,wrld,mnstr_loc):
+
+        potential_moves = self.build_tree(wrld,mnstr_loc, False)
+        highest_value = -math.inf
+        move_to_take = (0,0)
+        for aMove in potential_moves:
+            if aMove[1]>highest_value:
+                highest_value = aMove[1]
+                move_to_take = aMove[0]
+        return move_to_take
+
+    def build_tree(self,wrld,mnstr_loc, isMiniMax):
+        chtr_loc = (self.x,self.y)
+        #chance_node list of tuples (action, expected value)
+        chance_node: list [tuple(tuple, float)] = []
+
+        pot_chtr_moves = self.get_possible_moves(wrld,chtr_loc,False,True)
+
+        for Cmove in pot_chtr_moves:
+            if(not self.blast_radius(self.bomb_position, Cmove)):
+                utilities = self.build_T_nodes(wrld,mnstr_loc, Cmove)
+
+                #print(utilities)
+                probability = ((1)/len(utilities))
+                expectedValue= 0
+                if(isMiniMax):
+                    chance_node.append((Cmove, min(utilities)))
+                else:
+                    for aReward in utilities:
+                        expectedValue += (aReward*probability)
+                    chance_node.append((Cmove,expectedValue))
+        return chance_node
+
+    def build_T_nodes(self,wrld,mnstr_loc, Cmove):
+        chtr_loc = (self.x,self.y)
+        #C_loc_moved = (chtr_loc[0] + Cmove[0], chtr_loc[1]+Cmove[1])
+        C_loc_moved = Cmove
+
+        utilities = []
+        start_distance =self.get_Hn(chtr_loc, mnstr_loc)
+
+        pot_mnstr_moves = self.get_possible_moves(wrld,mnstr_loc,True,False)
+        for Mmove in pot_mnstr_moves:
+            M_loc_moved = (mnstr_loc[0]+Mmove[0],mnstr_loc[1]+Mmove[1])
+            new_dist = self.get_Hn(C_loc_moved, M_loc_moved)
+            if new_dist <= 2:
+                utilities.append(-10)
             else:
-                 print('need replan')
-                 #self.move_count = 0
-                 self.path_plan = True
+                utilities.append(2*(new_dist-start_distance))
+        return utilities
 
-  
-
-    def A_star(self,wrld, start, goal):
-        self.move_count = 0
+    def A_star(self,wrld):
+        start = (self.x,self.y)
+        goal = wrld.exitcell
         frontier = PriorityQueue()
-        frontier.put(start, 0)
+        frontier.put(start,0)
         came_from = {}  
         cost_incurred = {}  
         came_from[start] = None
         cost_incurred[start]=0
 
         while not frontier.empty():
-            current = frontier.get()
-            #print(current)
+            current  = frontier.get()
             
             if current == goal:
                 break
             
-            for next in self.get_neighbors_8(wrld, current):
-                if not wrld.wall_at(next[0], next[1]):
-                    new_cost = cost_incurred[current] + self.get_Gn(wrld,current,next)
+            for next in self.get_possible_moves(wrld,current,False, True):
+                new_cost = cost_incurred[current] + self.get_Gn(current,next)
                 
-                    if next not in cost_incurred or new_cost < cost_incurred[next]:
-                        cost_incurred[next] = new_cost
-                        priority = new_cost + self.get_Hn(goal, next)
-                        frontier.put(next, priority)
-                        came_from[next] = current
+                if next not in cost_incurred or new_cost < cost_incurred[next]:
+                    cost_incurred[next]= new_cost
+                    priority = new_cost + self.get_Hn(goal, next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
                     
         return came_from, cost_incurred
     
-    def get_neighbors_4(self, wrld, current):
-        neighbors = []
-        right_x = current[0]+1
-        left_x = current[0]-1
-        up_y = current[1]-1
-        down_y = current[1]+1
-        if right_x <  wrld.width():
-            #if not wrld.wall_at(right_x, current[1]):
-                neighbors.append((right_x, current[1]))
-        if  0 <= left_x:
-            #f not wrld.wall_at(left_x, current[1]):
-                neighbors.append(( left_x, current[1])) 
-        if 0 <= up_y:
-            #if not wrld.wall_at(current[0], up_y):
-                neighbors.append((current[0], up_y))
-        if down_y < wrld.height():
-            #print(current[0], down_y)
-            #if not wrld.wall_at(current[0], down_y):
-                neighbors.append((current[0], down_y))   
-        return(neighbors)
+    def get_possible_moves(self,wrld, loc, want_moves, isC):
+        neighbors = []   
+
+        for dx in [-1, 0, 1]:
+            # Avoid out-of-bounds access
+            if ((loc[0] + dx >= 0) and (loc[0]  + dx < wrld.width())):
+                for dy in [-1, 0, 1]:
+                    # Avoid out-of-bounds access
+                    if ((loc[1] + dy >= 0) and (loc[1] + dy < wrld.height())):
+                        if isC:
+                            if((wrld.exit_at(loc[0]  + dx, loc[1] + dy) or
+                                wrld.empty_at(loc[0]  + dx, loc[1] + dy)) and
+                                ((loc[0]  + dx, loc[1] + dy)!= (loc[0] ,loc[1]))):
+                                    if want_moves:
+                                        neighbors.append((dx, dy))
+                                    else:
+                                        neighbors.append((loc[0] + dx, loc[1] + dy))
+                            else:
+                                continue
+                        else:
+                            if((not wrld.wall_at(loc[0]  + dx, loc[1] + dy)) and
+                                ((loc[0]  + dx, loc[1] + dy)!= (loc[0] ,loc[1]))):
+                                    if want_moves:
+                                        neighbors.append((dx, dy))
+                                    else:
+                                        neighbors.append((loc[0] + dx, loc[1] + dy))
+
+        return neighbors
     
-    def get_neighbors_8(self, wrld, current):
-        #print(current)
-        neighbors = self.get_neighbors_4(wrld, current)
-        right_x = current[0]+1
-        left_x = current[0]-1
-        up_y = current[1]-1
-        down_y = current[1]+1
-        if (right_x < wrld.width()) and (up_y >= 0):
-            neighbors.append((right_x, up_y))
-        if (right_x < wrld.width()) and (down_y < wrld.height()):
-            neighbors.append((right_x, down_y)) 
-        if  (left_x >= 0) and (up_y >= 0):
-            neighbors.append((left_x, up_y)) 
-        if  (left_x >= 0) and (down_y < wrld.height()):
-            neighbors.append((left_x, down_y))  
-        #print(neighbors)
-        return(neighbors)
     
     def extract_move(self, next):
         print('next: ', next)
-        print('self: ', self.x, self.y)
         return next[0]-self.x, next[1]-self.y
 
      # the G(n) is the manhattan distance or right angle distance distance
-    def get_Gn(self, wrld, goal, next):
-        return abs(goal[0]-next[0]) + abs(goal[1]-next[1])
+    def get_Gn(self, current, next):
+        return abs(current[0]-next[0]) + abs(current[1]-next[1])
 
     # the h(n) is the euclidean_distance or straight line distance
     def get_Hn(self, goal, next):
         return abs(pow((goal[0]-next[0]), 2) + pow((goal[1]-next[1]), 2))
 
-    def get_path(self, came_from, start, goal):
+    """
+    Checks if a monster is in a given proximity to the character. If the monster
+    is in proximity, return location of the monster. Else, return False
+    """
+    def is_monster_in_proximity(self,wrld):
+        for dx in range(-self.depth,self.depth,1):
+            # Avoid out-of-bounds access
+            if ((self.x + dx >= 0) and (self.x + dx < wrld.width())):
+                for dy in range(-self.depth,self.depth,1):
+                    # Avoid out-of-bounds access
+                    if ((self.y + dy >= 0) and (self.y + dy < wrld.height())):
+                        if wrld.monsters_at(self.x + dx, self.y + dy):
+                            return (True, (self.x + dx, self.y + dy))
+        return (False,(0,0))       
+
+
+    def get_path(self, came_from, wrld):
+        start = (self.x, self.y)
+        goal = wrld.exitcell
         current = goal
         path=[]
         if goal not in came_from:
@@ -142,29 +217,22 @@ class TestCharacter(CharacterEntity):
         while current != start:
             self.set_cell_color(current[0], current[1],Fore.RED + Back.GREEN)
             path.append(current)
-            current = came_from[current]
+            current= came_from[current]
         # path.append(start)
         path.reverse()
         return path
     
-    def check_3_spaces(self, wrld):
-        for current in self.get_neighbors_8(wrld, (self.x, self.y)):
-            for current2 in self.get_neighbors_8(wrld, (current)):
-                for current3 in self.get_neighbors_8(wrld, (current2)):
-
-                    if(wrld.monsters_at(current3[0], current3[1]) != None):
-                        print('found monster')
-                        monster_pos = current3
-                        return monster_pos
+    def blast_radius(self, bomb_pos, move):
+        #print(bomb_pos)
+        #print(move)
+        if(not bomb_pos):
+            return False
+        for dx in range(-4, 4, 1):
+            radius = (bomb_pos[0] + dx, bomb_pos[1])
+            if ((move[0] == radius[0]) & (move[1] == radius[1])):
+                return True
+        for dy in range(-4, 4, 1):  
+            radius = (bomb_pos[0], bomb_pos[1] + dy)   
+            if ((move[0] == radius[0]) & (move[1] == radius[1])):
+                return True
         return False
-    
-    def run(self, wrld, status):
-        dx, dy = self.x - status[0], self.y - status[1]
-        
-        #dx, dy = self.move_no_wall(wrld, dx, dy)
-        print('run direction: ', dx, dy)
-        self.move(dx, dy)
-        self.path_plan = True
-
-    def move_no_wall(self, wrld, dx, dy):
-        pass
